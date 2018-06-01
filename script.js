ff67f6f9de3f1e3d7bbec80a50adb12517ed8491
Jason Wu <lujason2015@gmail.com>
2018-05-31 21:24:25 -0400

    display items

--- script.js
+++ script.js
@@ -1,24 +1,77 @@
+var defaultUsers = ["dreamhackcs", "skyzhar", "freecodecamp", 
+"faceittv", "comster404", "brunofin", "terakilobyte", "robotcaleb",
+"sheevergaming", "esl_sc2", "ogamingsc2", "jacksofamerica"];
+var responseObjArray = [];
+var domain = "https://api.twitch.tv/kraken/";
+
 function onloadScript(){
-    
-var url = 'https://api.twitch.tv/kraken/users/FreeCodeCamp';
-// var data = {username: 'example'};
 
-//To get access token
-// POST https://id.twitch.tv/oauth2/token
-//     ?client_id=<your client ID>
-//     &client_secret=<your client secret>
-//     &grant_type=client_credentials
-//     &scope=<space-separated list of scopes>
-
-fetch(url, {
-  method: 'POST', // or 'PUT'
-//   body: JSON.stringify(data), // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/json'
+  for(i=0; i<defaultUsers.length; i++){
+  callAPI(domain, "users", defaultUsers[i], handler);
   }
-}).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => console.log('Success:', response));
 
+}
+
+function handler(response){
+  pushValue(response);
+  displayListItem(response);
+}
+
+function displayListItem(object){
+  var list = document.getElementById("streamingList")
+   if(object.error == undefined){
+      var listItem = document.createElement("LI");
+      var listLink = document.createElement("A");
+      listItem.textContent = object.display_name;
+      // listLink.setAttribute("href", arrayToDisplay[i].display_name);
+      listItem.appendChild(listLink);
+      list.appendChild(listItem);
+      // console.log("has value");
+    }else{
+      
+    }
+}
+
+// function displayStreamingList(arrayToDisplay){
+//   var list = document.getElementById("streamingList")
+//   for (i=0; i<arrayToDisplay.length; i++){
+    
+//     if(arrayToDisplay[i].error == undefined){
+//       var listItem = document.createElement("LI");
+//       var listLink = document.createElement("A");
+//       listItem.textContent = arrayToDisplay[i].display_name;
+//       // listLink.setAttribute("href", arrayToDisplay[i].display_name);
+//       listItem.appendChild(listLink);
+//       list.appendChild(listItem);
+//       console.log("has value");
+//     }
+
+//   }
+// }
+
+function pushValue(value){
+  responseObjArray.push(value);
+}
+
+function callAPI(domain,key,value,callBack){
+  
+  var url = domain + key +"/"+value;
+  console.log(url);
+  
+    fetch(url, {
+    method: 'GET', // or 'PUT'
+  //body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Client-ID': '9m474lggbg8veapiw06p8r6px3w832'
+    }
+  }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(function(response){
+      // responseObjArray.push(response);
+      handler(response);
+      console.log('Success:', response); 
+    }
+  );
+  
 }
 
